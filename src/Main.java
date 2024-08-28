3daa097f38f074f00a8ff80a6d685836710cc4fb
IGabrielMT <iangmartinezt@gmail.com>
2024-08-28 16:12:33 -0500

    Main method cleaned

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,5 @@ import co.edu.uptc.models.SimpleList;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleList sl = new SimpleList();
-        sl.add(1);
-        sl.add(2);
-        sl.add(3);
-        System.out.println(sl.size());
-        for (Object o : sl.toArray()) {
-            System.out.println(o);
-        }
-        System.out.println(sl.sum());
     }
 }
