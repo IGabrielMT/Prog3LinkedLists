be4ad35c132218ff4165d4eaac873dacbae72525
IGabrielMT <iangmartinezt@gmail.com>
2024-09-03 16:33:25 -0500

    Created class Car

---
+++ src/co/edu/uptc/pojos/Car.java
@@ -0,0 +1,12 @@
+package co.edu.uptc.pojos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Car {
+    private String marca;
+    private String modelo;
+    private String color;
+}
