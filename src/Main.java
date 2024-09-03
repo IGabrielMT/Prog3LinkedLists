38342a8aae28c64ddfdc72d2281161c45047606e
IGabrielMT <iangmartinezt@gmail.com>
2024-09-03 17:13:44 -0500

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,25 @@
+import co.edu.uptc.models.CarComparator;
 import co.edu.uptc.models.SimpleList;
-import co.edu.uptc.models.SumNums;
-
-import java.util.ArrayList;
-import java.util.List;
+import co.edu.uptc.pojos.Car;
 
 public class Main {
     public static void main(String[] args) {
-        test();
-    }
-    private static void test(){
-        Main main = new Main();
-        List<Integer> nodeList = main.createNodeList();
-        List<Integer> arrayList = main.createArrayListNums();
-        long nodeListTime = main.measureSumTime(nodeList);
-        long arrayListTime = main.measureSumTime(arrayList);
+        SimpleList<Car> cars = new SimpleList<>();
+        cars.add(new Car("Chevrolet", 2020, "Red"));
+        cars.add(new Car("Mazda", 2021, "Blue"));
+        cars.add(new Car("Renault", 2019, "Black"));
+        cars.add(new Car("Toyota", 2022, "White"));
+        cars.add(new Car("Ford", 2018, "Yellow"));
+        System.out.println("Sin ordenar por marca \n");
+        for (Car car : cars) {
+            System.out.println(car.brand());
+        }
+        cars.sort(CarComparator.getBrandComparator());
+        System.out.println("Ordenados por marca \n");
+        for (Car car : cars) {
+            System.out.println(car.brand());
+        }
 
-        System.out.println("Time taken to sum SimpleList: " + nodeListTime + " nano seconds");
-        System.out.println("Time taken to sum ArrayList: " + arrayListTime + " nano seconds");
-    }
-    private List<Integer> createArrayListNums(){
-        List<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i < 500; i++) {
-            arrayList.add((int) (Math.random()*10000));
-        }
-        return arrayList;
-    }
-    private List<Integer> createNodeList(){
-        List<Integer> nodeList = new SimpleList<>();
-        for (int i = 0; i < 500; i++) {
-            nodeList.add((int) (Math.random()*10000));
-        }
-        return nodeList;
-    }
-    private long measureSumTime(List<Integer> list) {
-        SumNums sumNums = new SumNums();
-        System.out.println("Sum: " + sumNums.sumLists(list));
-        long startTime = System.nanoTime();
-        sumNums.sumLists(list);
-        long endTime = System.nanoTime();
-        return endTime - startTime;
     }
 
 
